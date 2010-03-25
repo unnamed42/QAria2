b3a6fd69e8f00a8437236a67ea4a1f7c4576c7eb
drswinghead <drswinghead@cb2a29f8-c037-0410-8809-e9d9649b9b1d>
2010-03-25 14:46:55 +0000

    change svn structure for tags

---
+++ trunk/karia2.pro
@@ -0,0 +1,12 @@
+HEADERS     = karia2.h settings.h
+SOURCES     = karia2.cpp main.cpp
+FORMS       = karia2.ui
+TRANSLATIONS = karia2_en.ts \
+			  karia2_it.ts
+
+# install
+target.path = karia2
+sources.files = $$SOURCES $$HEADERS $$RESOURCES $$FORMS *.pro
+sources.path = .
+INSTALLS += target sources
+RESOURCES = karia2.qrc
---
+++ trunk/translations/karia2_en.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>karia2</name>
+    <message>
+        <location filename="karia2.cpp" line="173"/>
+        <source>Downloading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.cpp" line="200"/>
+        <source>karia2 Open Url...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.cpp" line="201"/>
+        <source>URL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.cpp" line="213"/>
+        <source>karia2 Set Download Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="86"/>
+        <source>karia2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="260"/>
+        <source>file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="296"/>
+        <source>options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="274"/>
+        <source>help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="287"/>
+        <source>Qt::WindowStaysOnTopHint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="331"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="495"/>
+        <source>Resume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="351"/>
+        <source>Remove Completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="361"/>
+        <source>Stop </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="371"/>
+        <source>Destination Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="394"/>
+        <source>segmets: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="450"/>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="455"/>
+        <source>1 segment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="460"/>
+        <source>2 segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="465"/>
+        <source>3 segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="470"/>
+        <source>4 segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="475"/>
+        <source>5 segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="480"/>
+        <source>n segments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="485"/>
+        <source>destination folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="490"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="karia2.ui" line="500"/>
+        <source>about</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
