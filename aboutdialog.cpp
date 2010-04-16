cfd022d50edaed886bff79aed8886205e0970416
drswinghead <drswinghead@cb2a29f8-c037-0410-8809-e9d9649b9b1d>
2010-04-16 13:20:33 +0000

    this branch becomes main branch

---
+++ aboutdialog.cpp
@@ -0,0 +1,36 @@
+// aboutdialog.cpp --- 
+// 
+// Author: liuguangzhao
+// Copyright (C) 2007-2010 liuguangzhao@users.sf.net
+// URL: 
+// Created: 2010-04-06 13:22:27 +0800
+// Version: $Id$
+// 
+
+#include <QtGui>
+
+#include "aboutdialog.h"
+
+AboutDialog::AboutDialog(QWidget *parent)
+    : QDialog(parent)
+{
+	ui.setupUi(this);
+
+	//
+	QObject::connect(ui.pushButton,SIGNAL(clicked()),this,SLOT(changeWindowSize()));
+}
+
+AboutDialog::~AboutDialog()
+{
+}
+
+void AboutDialog::changeWindowSize()
+{
+	if (ui.pushButton->isChecked()) {
+		this->showFullScreen();
+		ui.pushButton->setText(tr("&Normal"));
+	} else {
+		this->showNormal();
+		ui.pushButton->setText(tr("&FullScreen"));
+	}
+}
